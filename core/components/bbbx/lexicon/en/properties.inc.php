0d08ee1481c4af9e18fcb032554777cef5ed8248
goldsky <goldsky.milis@gmail.com>
2016-04-14 20:03:50 +0700

    added properties lexicon

---
+++ core/components/bbbx/lexicon/en/properties.inc.php
@@ -0,0 +1,19 @@
+<?php
+
+$_lang['prop_limit_desc'] = 'If provided, limit the query. Used for getPage. Default: empty';
+$_lang['prop_offset_desc'] = 'If provided, offset the query. Used for getPage. Default: empty';
+$_lang['prop_totalVar_desc'] = 'Placeholder\'s name for "total" value. Used for getPage. Default: total';
+$_lang['prop_sortBy_desc'] = 'Field to sort by. Default: id';
+$_lang['prop_sortDir_desc'] = 'Order to sort by. Default: desc';
+$_lang['prop_allDates_desc'] = 'If it\'s "yes", this snippet will show all starting and ending dates. Default: 0';
+$_lang['prop_where_desc'] = 'JSON format for custom "where" condition in query. Default: empty';
+$_lang['prop_getMeetings_tplItem_desc'] = 'Chunk\'s name for item/row. Default: "meeting/item" (to parse {core_path}components/bbbx/elements/chunks/meeting/item.chunk.tpl)';
+$_lang['prop_getMeetings_tplWrapper_desc'] = 'Chunk\'s name for wrapper. Default: "meeting/wrapper" (to parse {core_path}components/bbbx/elements/chunks/meeting/wrapper.chunk.tpl)';
+$_lang['prop_getMeetings_phsPrefix_desc'] = 'Placeholder\'s prefix. Default: "bbbx.meeting."';
+$_lang['prop_itemSeparator_desc'] = 'HTML code to separate item\'s template. Default: "\n"';
+$_lang['prop_contextKey_desc'] = 'Context key to scope the listing. Default: empty (all contexts)';
+$_lang['prop_meetingId_desc'] = 'Comma separated BBB\'s meeting ID (not database\'s auto increment ID). Required. Default: empty.';
+$_lang['prop_getRecordings_tplItem_desc'] = 'Chunk\'s name for item/row. Default: "recording/item" (to parse {core_path}components/bbbx/elements/chunks/recording/item.chunk.tpl)';
+$_lang['prop_getRecordings_tplWrapper_desc'] = 'Chunk\'s name for wrapper. Default: "recording/wrapper" (to parse {core_path}components/bbbx/elements/chunks/recording/wrapper.chunk.tpl)';
+$_lang['prop_getRecordings_phsPrefix_desc'] = 'Placeholder\'s prefix. Default: "bbbx.recording."';
+$_lang['prop_authUser_desc'] = 'Check user\'s authetification. Default: empty';
